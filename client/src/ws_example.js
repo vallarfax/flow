a8090eb9a70b8d9b00d66712b07450ff6f7b4c84
Chris McCollister <oaeui@me.com>
2014-04-04 16:02:50 -0500

    Restructured the websocket server and added the ability for the client to send messages.

--- client/src/ws_example.js
+++ client/src/ws_example.js
@@ -3,6 +3,30 @@
  */
 
 
+var MessageView = React.createClass({
+	handleSend: function() {
+		var message = this.refs.message.getDOMNode().value.trim();
+
+		// Send message to server
+		this.props.sendMessage(message);
+
+		// Clear field
+		this.refs.message.getDOMNode().value = '';
+		return false;
+	},
+	render: function() {
+		return (
+			<form className="form-inline" role="form">
+				<div className="form-group">
+					<input type="text" className="form-control" placeholder="message" ref="message"/>
+				</div>
+				<button type="button" className="btn btn-default" onClick={this.handleSend}>Send</button>
+			</form>
+		);
+	}
+})
+
+
 var ConnectionStatusView = React.createClass({
 	render: function() {
 		return (
@@ -17,15 +41,16 @@ var AppView = React.createClass({
 		return (
 			<div className='container'>
 				<ConnectionStatusView status={this.props.status} />
+				<MessageView sendMessage={this.props.sendMessage} />
 			</div>
 		);
 	}
 });
 
 
-function render(status) {
+function render(status, sendMessage) {
 	React.renderComponent(
-		<AppView status={status} />,
+		<AppView status={status} sendMessage={sendMessage} />,
 		document.getElementById('app')
 	);
 }
@@ -33,14 +58,18 @@ function render(status) {
 
 function main() {
 	var socket = new WebSocket("ws://192.168.33.190:8765");
-	render('connecting');
+	var sendMessage = function(message) {
+		socket.send(message);
+	};
+
+	render('connecting', sendMessage);
 
 	socket.onopen = function (event) {
-		render('open');
+		render('open', sendMessage);
 	};
 
 	socket.onclose = function (event) {
-		render('closed');
+		render('closed', sendMessage);
 	};
 }
 
--- server/ws_server.py
+++ server/ws_server.py
@@ -1,16 +1,105 @@
+import signal
 import asyncio
+
 import websockets
 
 
-@asyncio.coroutine
-def hello(websocket, uri):
-    name = yield from websocket.recv()
-    print("< {}".format(name))
-    greeting = "Hello {}!".format(name)
-    print("> {}".format(greeting))
-    yield from websocket.send(greeting)
+class MyHandler:
+    def __init__(self, protocol):
+        self.protocol = protocol
 
-start_server = websockets.serve(hello, '0.0.0.0', 8765)
+    @asyncio.coroutine
+    def on_open(self):
+        print('connection opened.')
 
-asyncio.get_event_loop().run_until_complete(start_server)
-asyncio.get_event_loop().run_forever()
+    @asyncio.coroutine
+    def on_close(self):
+        print('connection closing...')
+
+    @asyncio.coroutine
+    def on_message(self, message):
+        print('< {}'.format(message))
+
+    @asyncio.coroutine
+    def _check_messages(self):
+        while True:
+            msg = yield from self.protocol.recv()
+
+            if not msg:
+                # that's all folks
+                return
+            yield from self.on_message(msg)
+
+
+class FlowProtocol(websockets.WebSocketServerProtocol):
+    def __init__(self, flow_handler):
+        self.flow_handler = flow_handler
+        super().__init__()
+
+    @asyncio.coroutine
+    def handler(self):
+        try:
+            uri = yield from self.handshake()
+        except Exception as exc:
+            self.writer.write_eof()
+            self.writer.close()
+            return
+
+        # print('handshake complete')
+
+        try:
+            flow_handler = self.flow_handler(self)
+        except Exception:
+            yield from self.fail_connection(1011)
+            return
+
+        yield from flow_handler.on_open()
+
+        # print('connection opened')
+
+        try:
+            yield from flow_handler._check_messages()
+        except Exception:
+            yield from self.fail_connection(1011)
+            return
+
+        yield from flow_handler.on_close()
+
+        # print('connection closed')
+
+        try:
+            yield from self.close()
+        except Exception as exc:
+            self.writer.write_eof()
+            self.writer.close()
+            return
+
+
+class App:
+    def __init__(self, handler, host, port):
+        self.handler = handler
+        self.host = host
+        self.port = port
+
+    def _start_server(self, loop, host, port):
+        f = loop.create_server(lambda: FlowProtocol(self.handler), host, port)
+        # f = loop.create_server(FlowProtocol, host, port)
+        s = loop.run_until_complete(f)
+
+    def run(self):
+        print('starting app')
+
+        loop = asyncio.get_event_loop()
+
+        # Ctrl-C handler
+        if signal is not None:
+            loop.add_signal_handler(signal.SIGINT, loop.stop)
+
+        self._start_server(loop, self.host, self.port)
+
+        loop.run_forever()
+
+
+if __name__ == '__main__':
+    app = App(MyHandler, '0.0.0.0', 8765)
+    app.run()
